acb5ee6b6c052e5670bf16f8536559ba5cf5b7b1
Stephen Lee <leestephen0320@gmail.com>
2024-11-02 16:14:42 -0700

    finished middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -5,6 +5,30 @@ interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (
+  req: Request, res: Response, next: NextFunction
+) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        // Check if the error is due to token expiration
+        if (err.name === 'TokenExpiredError') {
+          return res.status(401).json({ message: 'Token expired, please log in again.' });
+        }
+        return res.sendStatus(403); // Forbidden for other JWT errors
+      }
+
+      req.user = user as JwtPayload;
+      return next();
+    });
+  } else {
+    res.sendStatus(401); // Unauthorized
+  }
 };
